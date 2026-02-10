bbe8490020f5280f7ba770647b67eee3fa85f60f
Shannon Lee <shannon.lee@affinity.co>
2026-02-10 14:13:58 -0500

    add concat_str

--- src/test_mcp/main.py
+++ src/test_mcp/main.py
@@ -4,6 +4,10 @@ from mcp.server.fastmcp import FastMCP
 mcp = FastMCP("Test MCP")
 
 @mcp.tool()
+def concat_str(str1: str, str2: str) -> str:
+    return str1 + str2
+
+@mcp.tool()
 def hello_person(name: str) -> str:
     return f"Hello, {name}!"
 
